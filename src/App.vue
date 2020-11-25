bfffc526986b4c1e40ff8b8aba4399545a7c4ead
nik19ta <nikhvatov19@gmail.com>
2020-11-25 09:10:52 +0300

    Сделал запрет на копирование текст

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,15 @@ export default {
   name: 'App',
   components: {
     Main
+  },
+  mounted() {
+    document.ondragstart = noselect;
+    // запрет на перетаскивание
+    document.onselectstart = noselect;
+    // запрет на выделение элементов страницы
+    // document.oncontextmenu = noselect;
+    // запрет на выведение контекстного меню
+    function noselect() {return false;}
   }
 }
 </script>
