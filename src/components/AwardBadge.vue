f3d51dacd1c13595b6a6995b4546b6dccdafefe9
nik19ta <nikhvatov19@gmail.com>
2020-12-20 05:15:28 +0300

    Теперь вручить бэйдж можно только если ты можешь это сделать по иерархии вручения бэйджов

--- src/components/AwardBadge.vue
+++ src/components/AwardBadge.vue
@@ -6,17 +6,19 @@
                 <button class="close" v-on:click="closed" >×</button> <br/>
             </div>
             <div class="content" >
-                <select id='people' class="input select ">
+                <select id='people' v-model="user" class="input select "  @change="chenge_user">
                     <option :value='item.name' v-for='item in awardbadge_array.data' :key='item.name' >{{item.name}}</option>
                 </select>
 
                 <p>Выберите бейдж который хотите вручить</p>
-                <div class="assets_b" >
-                    <span v-for="i in 11" :key="i">
+                <div v-if="assets_is" class="assets_b" >
+                    <span v-bind:class="check(i) ? 'grn' : 'grn_false'"  v-for="i in 11" :key="i">
                         <img v-bind:class="badge == i ? 'active' : 'an'" @click="chenge_b(i)" class="icon" :src="require(`../assets/png_for_BulletinOfTheProject/${i}.svg`)" alt="">
+                        <span>
+                            <!-- {{descs[i]}} -->
+                        </span>
                     </span>
                 </div>
-
                 <div class="btns_inline" >
                     <button v-bind:class="badge < 1 ? 'dis_clacc' : ''" v-on:click="say" class="btn" >Вручить</button>
                     <button v-on:click="closed"  class="btn cancel " >Отмена</button>
@@ -35,16 +37,81 @@ export default {
     data: function () {
         return {
             text: '',
-            badge: 0
+            badge: 0,
+            user: '',
+            descs: [
+                'за результативность',
+                'за результативность',
+                'за самостоятельность',
+                'за внимательность',
+                'за организацию процесса',
+                'за ответственность',
+                'за отзывчивость',
+                'за опретивность',
+                'за вовлечённость',
+                'за мобильность',
+                'за командный дух',
+            ],
+            bages_count: [],
+            assets_is: true
         }
     },
     mounted() {},
     methods: {
+        check(numb) {
+            let numis = false;
+            let bages_count_ = this.bages_count;
+
+
+            console.log(bages_count_[0]);
+
+            for (let i = 0; i < bages_count_.length; i++) {
+                if (bages_count_[i] == numb) {
+                    numis = true
+                    return true
+                }
+            }
+            return numis
+        },
+        chenge_user() {
+            this.badge= 0
+            fetch(this.host + 'can_give_badge', {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    method: "POST",
+                    body: JSON.stringify({
+                        cookie: document.cookie.split('=')[1],
+                        to: this.user
+                    })
+                })
+                .then(response => response.text())
+                .then((response) => { 
+                    console.log(response)
+                    if (JSON.parse(response).status == 'you not a login') {
+                        alert('Вы не вошло на сайт, и не можете вручить бэйдж')
+                        this.user =' '
+                    } else if (JSON.parse(response).status == "you can't give a badge to yourself") {
+                        alert('Вы не можете дать бэйдж сами себе')
+                        this.user =' '
+                    } else if (JSON.parse(response).status == 'ok') {
+                        this.bages_count = JSON.parse(response).bages_count
+                        this.assets_is = false
+                        this.assets_is = true
+                    } else if (JSON.parse(response).status == 'countnull') {
+                        console.log('Вы не можете вручить бэйдж этому человеку')
+                    }
+                })
+                .catch(err => console.log(err))
+        },  
         chenge_b(data) {
-            if (this.badge == data) {
-                this.badge = 0;
-            } else {
-                this.badge = data;
+            if (this.check(data)) {
+                if (this.badge == data) {
+                    this.badge = 0;
+                } else {
+                    this.badge = data;
+                }
             }
         },
         say() {
@@ -58,7 +125,7 @@ export default {
                     },
                     method: "POST",
                     body: JSON.stringify({
-                        from: "user..",
+                        from: document.cookie.split('=')[1],
                         badge: this.badge,
                         to: document.querySelector('#people').value
                     })
@@ -231,7 +298,7 @@ form{
 .icon{
     width: 45px;
     cursor: pointer;
-    transition:width .3s, margin 0.2s;
+    transition:transform .25s;
 }
 .assets_b{
     display: flex;
@@ -245,11 +312,18 @@ form{
     text-align: center;
 }
 .active{
-    width: 65px;
-    margin-top: -10px;
+    /* width: 65px; */
+    transform: scale(1.35); 
+    /* margin-top: -10px; */
     margin-right: 0px;
 }
 .dis_clacc{
     color: gray;
+}
+.grn_false{
+    filter: grayscale(1)
+}
+.grn{
+    filter: grayscale(0)
 }
 </style>
